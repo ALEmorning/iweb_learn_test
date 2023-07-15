35c5e94c6ad455e40d586a52fd19585f2a8ddf80
luck <lz489234@gmail.com>
2023-07-15 16:15:56 +0800

    数组课后作业

---
+++ iweb_learn_test/src/com/iweb/homework/PlayerInfo.java
@@ -0,0 +1,88 @@
+package com.iweb.homework;
+
+import java.util.Scanner;
+
+/*
+*
+* 撰写一个简单的程序用于录入用户信息，并展示出来，要有简单的功能菜单
+ */
+public class PlayerInfo {
+    String[][] playerInfo = new String[5][];
+
+    Scanner scan = new Scanner(System.in);
+    int count = 1;
+    public static void main( String[] args ) {
+        PlayerInfo pi = new PlayerInfo();
+        pi.MainPage();
+
+
+    }
+    public void MainPage(){//功能主界面，供用户选择功能
+        int option;
+        System.out.println("欢迎来到用户界面：");
+        System.out.println("请选择功能：1、添加用户信息；2、查看所有用户信息；3、退出");
+        option = scan.nextInt();
+        switch ( option ){
+            case 1:
+                InsertInfo();
+                break;
+            case 2:
+                ShowInfo();
+                break;
+            case 3:
+                System.out.println("谢谢使用，您已成功退出。");
+                return;
+            default:
+                System.out.println("请选择正确的功能。");
+        }
+    }
+    public void InsertInfo(){
+        String name = new String();//用户相关信息，共三项：账号、密码、姓名
+        String password = new String();
+        String mingcheng = new String();
+        String option = new String();
+
+        if ( count == playerInfo.length - 1 ){//每次插入前，先对数组长度进行判定
+            String[][] temp = new String[playerInfo.length * 2][];//双倍扩充
+            //把原先数组数据找回
+            System.arraycopy(playerInfo,0,temp,0,playerInfo.length);
+            playerInfo = temp;//扩充数组
+        }
+
+        System.out.println("欢迎使用添加用户信息功能：");
+        System.out.print("请输入用户账号：\t");
+        name = scan.next();
+
+       // System.out.println();
+        System.out.print("请输入用户密码：\t");
+        password = scan.next();
+
+        //System.out.println();
+        System.out.print("请输入用户姓名：\t");
+        mingcheng = scan.next();
+
+        playerInfo[count++] = new String[]{name,password,mingcheng};//将用户输入值添加进数组
+
+        System.out.println("是否继续添加信息：y/n");
+        option = scan.next();
+        if ( "y".equals(option) ){
+            InsertInfo();//递归调用
+        }else {
+            System.out.println("您已退出！");
+            MainPage();
+        }
+    }
+
+
+    public void ShowInfo(){
+        playerInfo[0] = new String[]{"账号","密码","姓名"};//首行信息，给予用户提示，列名
+        System.out.println("用户信息：");
+        System.out.print("账号\t\t"+"\t"+"密码\t"+"\t\t"+"姓名\n");
+        for (int i = 1; i < playerInfo.length; i++) {
+            if (playerInfo[i] != null) {
+                System.out.print(playerInfo[i][0] + "\t\t" + playerInfo[i][1] + "\t\t" + playerInfo[i][2] + "\n");
+            }
+        }
+        MainPage();
+    }
+}
---
+++ iweb_learn_test/src/com/iweb/homework/PokerGameWork.java
@@ -0,0 +1,146 @@
+package com.iweb.homework;
+
+import java.util.Random;
+
+/*
+*
+* 1 使用二维数组重新改造扑克牌的发牌程序
+* 2 对玩家获得的牌进行由大到小的排序
+* 3 撰写一个简单的程序用于录入用户信息，并展示出来，要有简单的功能菜单
+ */
+public class PokerGameWork {
+    String[][] box = new String[54][];//全局变量，为一副扑克牌创建二维数组
+    //为玩家创建手牌二维数组
+    String[][] playA = new String[18][];
+    String[][] playB = new String[18][];
+    String[][] playC = new String[18][];
+
+    public static void main( String[] args ) {
+        PokerGameWork pw = new PokerGameWork();
+        pw.CreateCards();//创建扑克牌，将其存入box
+        pw.Publisher();//发牌
+        pw.ShowCards();//展示手牌
+
+    }
+
+    public void CreateCards(){//将所有牌存入二维数组
+        int count = 0;
+        for ( int i = 1 ;i<=13;i++ ){
+            String cn = "";
+            switch ( i ){
+                case 1:
+                    cn="A";
+                    break;
+                case 11:
+                    cn = "J";
+                    break;
+                case 12:
+                    cn = "Q";
+                    break;
+                case 13:
+                    cn = "K";
+                    break;
+                default:
+                    cn = i+"";
+            }
+            box[count++] = new String[] {"♠",cn};
+            box[count++] = new String[] {"♥",cn};
+            box[count++] = new String[] {"♣",cn};
+            box[count++] = new String[] {"♦",cn};
+
+        }
+        box[52] = new String[] {"♥小王","66"};
+        box[53] = new String[] {"♠大王","88"};
+
+
+        for (int i = 0; i < box.length; i++) {
+            System.out.print(box[i][0] + box[i][1] + "\t");
+            if ((i + 1) % 4 == 0) {
+                System.out.println();
+            }
+        }
+    }
+
+    public void Publisher(){//将牌按照 A B C 的形式随机发放
+        Random r = new Random();
+        int t_idx = 0;
+        while ( box.length > 0 ){
+            int idx = r.nextInt(box.length);
+            String[] card = box[idx];
+
+            switch ( box.length % 3 ){//三个一组，模3计算
+                case 0:
+                    playA[t_idx]=card;
+                    break;
+                case 2:
+                    playB[t_idx]=card;
+                    break;
+                case 1:// 1 为最后一个，因此1 结束后用户ID t_idx 加1
+                    playC[t_idx]=card;
+                    t_idx++;
+                    break;
+            }
+            //将牌移除
+            String[][] temp = new String[box.length-1][];//临时数组
+            System.arraycopy(box,0,temp,0,idx);//拷贝前半牌组
+            System.arraycopy(box,idx+1,temp,idx,temp.length-idx);//拷贝后半
+            box = temp;//替换原box
+        }
+    }
+
+    public void ShowCards(){
+        Sorter(playA);
+        Sorter(playB);
+        Sorter(playC);
+
+        System.out.print("\nPlayerA: ");
+        for(String[] every : playA) {
+            System.out.print(every[0]+every[1]+"\t");
+        }
+        System.out.print("\nPlayerB: ");
+        for(String[] every : playB) {
+            System.out.print(every[0]+every[1]+"\t");
+        }
+        System.out.print("\nPlayerC: ");
+        for(String[] every : playC) {
+            System.out.print(every[0]+every[1]+"\t");
+        }
+
+    }
+
+    public void Sorter(String[][] player) {//对玩家手牌进行冒泡排序，降序排列
+        for(int i = 0; i< player.length; i++) {
+            for(int j = player.length - 1; j > 0; j--) {
+                String[] c1 = player[j];
+                String[] c2 = player[j-1];
+
+                int a = covert(c1[1]);
+                int b = covert(c2[1]);
+
+                if(a > b) {
+                    player[j] = c2;
+                    player[j-1] = c1;
+                }
+
+            }
+        }
+    }
+
+    public int covert(String value){//将牌组中二维数组的数字为取出，转换格式进行比较
+        switch(value) {
+            case "A":
+                return 14;
+            case "2":
+                return 15;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            default:
+                return Integer.parseInt(value);
+        }
+    }
+
+}
