78425c81c492f97204593ac0c74dee17d02bcfa5
luck <lz489234@gmail.com>
2023-07-17 17:55:48 +0800

    利用类声明对象对象完成扑克牌的创建以及玩家手牌的生成

--- iweb_learn_test/src/com/iweb/homework/PokerGameWork.java
+++ iweb_learn_test/src/com/iweb/homework/PokerGameWork.java
@@ -1,5 +1,7 @@
 package com.iweb.homework;
 
+import com.iweb.learn0717.PokerCards;
+
 import java.util.Random;
 
 /*
@@ -9,18 +11,23 @@ import java.util.Random;
 * 3 撰写一个简单的程序用于录入用户信息，并展示出来，要有简单的功能菜单
  */
 public class PokerGameWork {
-    String[][] box = new String[54][];//全局变量，为一副扑克牌创建二维数组
-    //为玩家创建手牌二维数组
-    String[][] playA = new String[18][];
-    String[][] playB = new String[18][];
-    String[][] playC = new String[18][];
+    PokerCards[] pc = new PokerCards[54];//所有牌的存储盒子
+    PokerCards[] p1 = new PokerCards[18];//玩家1
+    PokerCards[] p2 = new PokerCards[18];//玩家2
+    PokerCards[] p3 = new PokerCards[18];//玩家3
+
+    int i =0;
+//    String[][] box = new String[54][];//全局变量，为一副扑克牌创建二维数组
+//    //为玩家创建手牌二维数组
+//    String[][] playA = new String[18][];
+//    String[][] playB = new String[18][];
+//    String[][] playC = new String[18][];
 
     public static void main( String[] args ) {
         PokerGameWork pw = new PokerGameWork();
-        pw.createCards();//创建扑克牌，将其存入box
+        pw.createCards();//创建扑克牌，将其存入pc
         pw.publisher();//发牌
         pw.showCards();//展示手牌
-
     }
 
     public void createCards(){//将所有牌存入二维数组
@@ -43,19 +50,26 @@ public class PokerGameWork {
                 default:
                     cn = i+"";
             }
-            box[count++] = new String[] {"♠",cn};
-            box[count++] = new String[] {"♥",cn};
-            box[count++] = new String[] {"♣",cn};
-            box[count++] = new String[] {"♦",cn};
+            pc[count++] = new PokerCards("♠",cn);
+            pc[count++] = new PokerCards("♥",cn);
+            pc[count++] = new PokerCards("♣",cn);
+            pc[count++] = new PokerCards("♦",cn);
+//            box[count++] = new String[] {"♠",cn};
+//            box[count++] = new String[] {"♥",cn};
+//            box[count++] = new String[] {"♣",cn};
+//            box[count++] = new String[] {"♦",cn};
 
         }
-        box[52] = new String[] {"♥小王","66"};
-        box[53] = new String[] {"♠大王","88"};
+        pc[52] = new PokerCards("♥小王","66");
+        pc[53] = new PokerCards("♠大王","88");
+//        box[52] = new String[] {"♥小王","66"};
+//        box[53] = new String[] {"♠大王","88"};
 
 
-        for (int i = 0; i < box.length; i++) {
-            System.out.print(box[i][0] + box[i][1] + "\t");
-            if ((i + 1) % 4 == 0) {
+        for (PokerCards p : pc) {
+            System.out.print(p.varity + p.number + "\t");
+            i++;
+            if ((i ) % 4 == 0) {
                 System.out.println();
             }
         }
@@ -64,62 +78,63 @@ public class PokerGameWork {
     public void publisher(){//将牌按照 A B C 的形式随机发放
         Random r = new Random();
         int t_idx = 0;
-        while ( box.length > 0 ){
-            int idx = r.nextInt(box.length);
-            String[] card = box[idx];
+        while ( pc.length > 0 ){
+            int idx = r.nextInt(pc.length);
+            PokerCards card = pc[idx];
 
-            switch ( box.length % 3 ){//三个一组，模3计算
+            switch ( pc.length % 3 ){//三个一组，模3计算
                 case 0:
-                    playA[t_idx]=card;
+                    p1[t_idx]=card;
                     break;
                 case 2:
-                    playB[t_idx]=card;
+                    p2[t_idx]=card;
                     break;
                 case 1:// 1 为最后一个，因此1 结束后用户ID t_idx 加1
-                    playC[t_idx]=card;
+                    p3[t_idx]=card;
                     t_idx++;
                     break;
             }
             //将牌移除
-            String[][] temp = new String[box.length-1][];//临时数组
-            System.arraycopy(box,0,temp,0,idx);//拷贝前半牌组
-            System.arraycopy(box,idx+1,temp,idx,temp.length-idx);//拷贝后半
-            box = temp;//替换原box
+            PokerCards[] temp = new PokerCards[pc.length-1];//临时数组
+            System.arraycopy(pc,0,temp,0,idx);//拷贝前半牌组
+            System.arraycopy(pc,idx+1,temp,idx,temp.length-idx);//拷贝后半
+            pc = temp;//替换原pc
         }
     }
 
     public void showCards(){
-        sorter(playA);
-        sorter(playB);
-        sorter(playC);
+        sorter(p1);
+        sorter(p2);
+        sorter(p3);
 
         System.out.print("\nPlayerA: ");
-        for(String[] every : playA) {
-            System.out.print(every[0]+every[1]+"\t");
+        for(PokerCards p : p1) {
+            System.out.print(p.varity+p.number+"\t");
         }
         System.out.print("\nPlayerB: ");
-        for(String[] every : playB) {
-            System.out.print(every[0]+every[1]+"\t");
+        for(PokerCards p : p2) {
+            System.out.print(p.varity+p.number+"\t");
         }
         System.out.print("\nPlayerC: ");
-        for(String[] every : playC) {
-            System.out.print(every[0]+every[1]+"\t");
+        for(PokerCards p : p3) {
+            System.out.print(p.varity+p.number+"\t");
         }
 
     }
 
-    public void sorter(String[][] player) {//对玩家手牌进行冒泡排序，降序排列
+    public void sorter(PokerCards[] player) {//对玩家手牌进行冒泡排序，降序排列
         for(int i = 0; i< player.length; i++) {
             for(int j = player.length - 1; j > 0; j--) {
-                String[] c1 = player[j];
-                String[] c2 = player[j-1];
+                PokerCards p ;
+                int c1 = covert(player[j].number);
+                int c2 = covert(player[j-1].number);
 
-                int a = covert(c1[1]);
-                int b = covert(c2[1]);
 
-                if(a > b) {
-                    player[j] = c2;
-                    player[j-1] = c1;
+
+                if(c1 > c2) {
+                    p = player[j] ;
+                    player[j] = player[j-1];
+                    player[j-1] = p;
                 }
 
             }
